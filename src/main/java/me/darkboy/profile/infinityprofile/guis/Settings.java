4a1c63b35d5afdc762fbf4a7a4b0b20f96c6e861
EatingSouls <timmy120403@gmail.com>
2019-07-24 19:27:52 +0200

    Aggiunto il rank

--- src/main/java/me/darkboy/profile/infinityprofile/guis/Settings.java
+++ src/main/java/me/darkboy/profile/infinityprofile/guis/Settings.java
@@ -8,6 +8,8 @@ import me.darkboy.profile.infinityprofile.api.util.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
+import java.util.HashSet;
+
 public class Settings implements InventoryProvider {
 
     public static InfinityInventory inv = InfinityInventory.builder()
@@ -17,6 +19,8 @@ public class Settings implements InventoryProvider {
             .title("          §bSettings")
             .build();
 
+    private HashSet<Player> hide = new HashSet<>();
+
     @Override
     public void init(Player player, InventoryContents contents) {
         contents.fillBorders(ClickableItem.empty(new ItemBuilder(Material.STAINED_GLASS_PANE).setDisplayName("§7✦ §b§lDream§c§lWars §7✦").build()));
@@ -24,6 +28,18 @@ public class Settings implements InventoryProvider {
 
     @Override
     public void update(Player player, InventoryContents contents) {
+        contents.set(1, 2, ClickableItem.empty(new ItemBuilder(Material.WATCH).setDisplayName("§aPlayer visibility").build()));
+        if(!hide.contains(player)) {
+            contents.set(2, 2, ClickableItem.empty(new ItemBuilder(Material.INK_SACK, (byte) 10).setDisplayName("§aVisibility Enabled").build()));
+        } else {
+            contents.set(2, 2, ClickableItem.empty(new ItemBuilder(Material.INK_SACK, (byte) 8).setDisplayName("§aVisibility Disabled").build()));
+        }
 
+
+
+/*        contents.set(1, 2, ClickableItem.empty(new ItemBuilder(Material.FEATHER).setDisplayName("§aFly").build()));
+        if(player.getAllowFlight()) {
+            contents.set()*/
+        }
     }
 }
